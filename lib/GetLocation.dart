19b6dddfe94b3abaf23803d860af4f882350756a
Muhammad Hanif <muhammad_hanif@telkomsel.co.id>
2022-10-10 09:35:04 +0700

    Upgrade to latest version and implement linter

--- lib/GetLocation.dart
+++ lib/GetLocation.dart
@@ -1,28 +1,27 @@
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
 
-class GetLocation{
+class GetLocation {
   double latitude;
   double longitude;
   String city;
 
   //get current location
-  Future<void> getCurrentLocation() async{
+  Future<void> getCurrentLocation() async {
     try {
-      Position position = await getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high);
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
       latitude = position.latitude;
       longitude = position.longitude;
 
       city = await getCityName(position.latitude, position.longitude);
-    }
-    catch(e){
+    } catch (e) {
       print(e);
     }
   }
 
   //get city name
-  Future<String> getCityName(double lat, double lon) async{
+  Future<String> getCityName(double lat, double lon) async {
     List<Placemark> placemark = await placemarkFromCoordinates(lat, lon);
     return placemark[0].locality;
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:weather_app/GetLocation.dart';
-import 'GetLocation.dart';
-import 'package:http/http.dart' as http;
 
 void main() => runApp(WeatherApp());
 
@@ -14,7 +13,7 @@ class WeatherApp extends StatefulWidget {
 }
 
 class _WeatherAppState extends State<WeatherApp> {
-  String apiKey = '44f83194f32b63da7b04a0fe956d0208';
+  String apiKey = '9fa3b94ceead0f42ad805323f52bcbf5';
   var city;
   var position;
   var description;
@@ -22,7 +21,6 @@ class _WeatherAppState extends State<WeatherApp> {
 
   @override
   void initState() {
-    // TODO: implement initState
     getCurrentLocation();
     super.initState();
   }
@@ -42,24 +40,26 @@ class _WeatherAppState extends State<WeatherApp> {
             // SizedBox(height: 50.0,),
             Container(
               margin: EdgeInsets.only(top: 50.0),
-              child: Text('You are in: ', style:
-                TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue
-                ),),
+              child: Text(
+                'You are in: ',
+                style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue),
+              ),
             ),
             Container(
               margin: EdgeInsets.symmetric(horizontal: 25.0),
               child: Row(
                 children: [
                   Container(
-                    child: Text(city == null ? 'Fetching location...' : city, style:
-                      TextStyle(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue[500]
-                      ),),
+                    child: Text(
+                      city == null ? 'Fetching location...' : city,
+                      style: TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[500]),
+                    ),
                   ),
                 ],
               ),
@@ -68,23 +68,26 @@ class _WeatherAppState extends State<WeatherApp> {
               margin: EdgeInsets.symmetric(vertical: 17.0, horizontal: 25.0),
               color: Colors.white,
               child: ListTile(
-                leading: Icon(Icons.wb_sunny, color: Colors.amber,),
-                title: Text('Temperature: ${temp == null ? '...' : '$temp °C'}'),
-                subtitle: Text('Status: ${description == null ? '...' : description}'),
+                leading: Icon(
+                  Icons.wb_sunny,
+                  color: Colors.amber,
+                ),
+                title:
+                    Text('Temperature: ${temp == null ? '...' : '$temp °C'}'),
+                subtitle: Text(
+                    'Status: ${description == null ? '...' : description}'),
               ),
             ),
             Container(
               child: Center(
-                child: FlatButton(
+                child: ElevatedButton(
                   child: Text('Get weather info'),
-                  color: Colors.blue[500],
-                  textColor: Colors.white,
-                  onPressed: (){
+                  onPressed: () {
                     getTemp(position['latitude'], position['longitude']);
                   },
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -92,19 +95,18 @@ class _WeatherAppState extends State<WeatherApp> {
   }
 
   //display images based on current time
-  displayImage(){
+  displayImage() {
     var now = DateTime.now();
     final currentTime = DateFormat.jm().format(now);
-    if(currentTime.contains('AM')){
+    if (currentTime.contains('AM')) {
       return Image.asset('images/dayTime.jpg');
-    }
-    else if(currentTime.contains('PM')){
+    } else if (currentTime.contains('PM')) {
       return Image.asset('images/nightTime.jpg');
     }
   }
 
   //get location
-  void getCurrentLocation() async{
+  void getCurrentLocation() async {
     GetLocation getLocation = GetLocation();
     await getLocation.getCurrentLocation();
     setState(() {
@@ -117,14 +119,14 @@ class _WeatherAppState extends State<WeatherApp> {
   }
 
   //get current temp
-  Future<void> getTemp(double lat, double lon) async{
-    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric');
-
+  Future<void> getTemp(double lat, double lon) async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric');
     var dataDecoded = jsonDecode(response.body);
+    print(dataDecoded);
     setState(() {
       description = dataDecoded['weather'][0]['description'];
       temp = dataDecoded['main']['temp'];
     });
   }
 }
-
